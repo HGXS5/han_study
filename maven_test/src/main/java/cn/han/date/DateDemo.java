99bcccd37e9b79631a30fc5cd4b9ec6ede16e4e6
han <15638258963@163.com>
2022-12-06 17:42:26 +0800

    date与localdatetime转换

--- maven_test/src/main/java/cn/han/date/DateDemo.java
+++ maven_test/src/main/java/cn/han/date/DateDemo.java
@@ -1,10 +1,7 @@
 package cn.han.date;
 
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -15,16 +12,36 @@ import java.util.*;
  */
 public class DateDemo {
     public static void main(String[] args) {
+        /*LocalDateTime转换城date*/
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("最初time:"+now);
+        ZonedDateTime zonedDateTime = now.atZone(zoneId);
+        Instant instant = zonedDateTime.toInstant();
+        Date from = Date.from(instant);
+        System.out.println(from);
+    }
+
+    public static void test6() {
+        /*date转换成LocalDateTime*/
+        Date time = new Date();
+        System.out.println("原始的time:" + time);
+        Instant instant = time.toInstant();
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime dateTime = instant.atZone(zoneId).toLocalDateTime();
+        System.out.println(dateTime);
+    }
+
+    public static void test5() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         System.out.println(formatter.format(now));
         LocalDate date = LocalDate.now();
         DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         System.out.println(pattern.format(date));
-
     }
 
-    public static void test4(){
+    public static void test4() {
         /*间隔日期*/
         List<String> dates = new ArrayList<>();
 
@@ -35,10 +52,10 @@ public class DateDemo {
         LocalDate startDate = LocalDate.parse(startTime);
         LocalDate endDate = LocalDate.parse(endTime);
 
-        Period next = Period.between(startDate,endDate);
+        Period next = Period.between(startDate, endDate);
         int days = next.getDays();
         dates.add(startTime);
-        while (days>0){
+        while (days > 0) {
             startDate = startDate.minusDays(-1);
             String nextDate = formatter.format(startDate);
             dates.add(nextDate);
@@ -47,13 +64,15 @@ public class DateDemo {
 
         System.out.println(dates.toString());
     }
-    public static void test3(){
+
+    public static void test3() {
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String startTime = formatter.format(date);
         System.out.println(startTime);
     }
-    public void test1(){
+
+    public void test1() {
         //        Long nowData = (new Date()).getTime();
 //        Long startTime = nowData-(1209600000+1000 * 60 * 60 * 24 * 7);
 ////        Long startTime = nowData-(1209600000);
@@ -66,7 +85,8 @@ public class DateDemo {
 
         System.out.println(localDateTime);
     }
-    public static void test2(){
+
+    public static void test2() {
         Instant now = Instant.now();
         LocalDateTime dateTime = LocalDateTime.now();
         DateTimeFormatter basicIsoDate = DateTimeFormatter.BASIC_ISO_DATE;
